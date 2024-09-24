d9d118a8f3fd8c158b9f97e53b6ebf900b825e1d
Marcos Duran <Marcos_Duran@ucne.edu.do>
2024-09-23 20:52:17 -0400

    Add project files.

---
+++ DAL/Context.cs
@@ -0,0 +1,11 @@
+﻿using MarcosDuran_Ap1_P1.Components.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
+
+namespace MarcosDuran_Ap1_P1.Components.DAL;
+
+public class Contexto(DbContextOptions<Contexto> options) : DbContext(options)
+{
+    public DbSet<Registro> Registro { get; set; }
+
+}
---
+++ Models/Registro.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MarcosDuran_Ap1_P1.Components.Models
+{
+    public class Registro
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
