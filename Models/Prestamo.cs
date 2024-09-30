c462ce5ed7ae703d32bc337e5a402cc97c782f89
Marcos Duran <Marcos_Duran@ucne.edu.do>
2024-09-30 18:28:46 -0400

    Creando nuevo modelo Prestamo y eliminando viejo modelo registro

---
+++ Models/Prestamo.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MarcosDuran_Ap1_P1.Models;
+
+public class Prestamo
+{
+    [Key ] 
+    public int DeudorId { get; set; }
+    [Required]
+    public string Deudor { get; set; }
+    [Required]
+    public string Concepto { get; set; }
+    [Required]
+    public int Monto { get; set; }
+}
